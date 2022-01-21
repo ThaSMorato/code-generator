5ff212ec92ba79e5560a7848d9c8d98aa2d4aa48
Thales Morato <tha.s.morato@gmail.com>
2022-01-21 01:16:10 -0300

    chore: init project

---
+++ jest.config.mjs
@@ -0,0 +1,18 @@
+export default {
+  coverageDirectory: "coverage",
+  coverageProject: "v8",
+  coverageReporters: ["json", "text", "lcov", "clover"],
+  collectCoverageFrom: ["src/**/*.js", "!src/index.js"],
+  coverageThreshold: {
+    global: {
+      branch: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  maxWorkers: "50%",
+  testEnvironment: "node",
+  watchPathIgnorePatterns: ["node_modules"],
+  transformIgnorePatterns: ["node_modules"],
+};
