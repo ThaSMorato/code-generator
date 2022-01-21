4c17ae7946bc70bc50b7270f1ab7fcde3ee3d628
Thales Morato <tha.s.morato@gmail.com>
2022-01-21 12:27:16 -0300

    feat: create files

---
+++ src/createFiles.js
@@ -0,0 +1,46 @@
+import fsPromises from "fs/promises";
+import fs from "fs";
+import templates from "./templates/index.js";
+
+const defaultDependencies = (layer, componentName) => {
+  const dependencies = new Map([
+    ["repository", []],
+    ["service", [`${componentName}Repository`]],
+    ["factory", [`${componentName}Repository`, `${componentName}Service`]],
+  ]);
+
+  return dependencies.get(layer);
+};
+
+const executeWrites = async (pendingFilesToWrite) => {
+  return Promise.all(
+    pendingFilesToWrite.map(({ fileName, txtFile }) => fsPromises.writeFile(fileName, txtFile))
+  );
+};
+export async function createFiles({ mainPath, defaultMainFolder, layers, componentName }) {
+  const keys = Object.keys(templates);
+
+  const pendingFilesToWrite = [];
+
+  for (const layer of layers) {
+    const chosenTemplate = keys.find((key) => key.includes(layer));
+
+    if (!chosenTemplate) return { error: "The chosen layer doesnt have a template" };
+
+    const template = templates[chosenTemplate];
+
+    const targetFolder = `${mainPath}/${defaultMainFolder}/${layer}`;
+
+    const dependencies = defaultDependencies(layer, componentName);
+
+    const { fileName, template: txtFile } = template(componentName, ...dependencies);
+
+    const fileNameJs = `${targetFolder}/${fileName}.js`;
+
+    pendingFilesToWrite.push({ fileName: fileNameJs, txtFile });
+  }
+
+  await executeWrites(pendingFilesToWrite);
+
+  return { success: true };
+}
